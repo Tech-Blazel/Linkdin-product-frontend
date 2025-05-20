92a4a2aa1111371bc53c302f807a5853ce8b499e
Akash Sharma <akashsharma@192.168.1.23>
2025-05-20 05:58:35 +0530

    fix: fixed a layout issue

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -8,27 +8,29 @@ const Layout: React.FC = () => {
   const [isSidebarOpen, setIsSidebarOpen] = React.useState(false);
 
   return (
-    <div className="flex h-screen overflow-hidden relative">
-      <aside
-        className={twMerge(
-          "absolute -left-full md:static md:block transition-all duration-500",
-          isSidebarOpen && "block left-0"
-        )}
-      >
-        <Sidebar
-          isSidebarOpen={isSidebarOpen}
-          setIsSidebarOpen={setIsSidebarOpen}
-        />
-      </aside>
+    <div className="max-w-[2500px] mx-auto w-full px-4">
+      <div className="flex h-screen overflow-hidden relative">
+        <aside
+          className={twMerge(
+            "absolute -left-full md:static md:block transition-all duration-500",
+            isSidebarOpen && "block left-0"
+          )}
+        >
+          <Sidebar
+            isSidebarOpen={isSidebarOpen}
+            setIsSidebarOpen={setIsSidebarOpen}
+          />
+        </aside>
 
-      <div className="flex flex-col flex-1 overflow-hidden">
-        <Header
-          isSidebarOpen={isSidebarOpen}
-          setIsSidebarOpen={setIsSidebarOpen}
-        />
-        <main className="flex-1 overflow-auto bg-gray-100 p-6 md:p-10">
-          <Outlet /> {/* ✅ renders nested route components */}
-        </main>
+        <div className="flex flex-col flex-1 overflow-hidden">
+          <Header
+            isSidebarOpen={isSidebarOpen}
+            setIsSidebarOpen={setIsSidebarOpen}
+          />
+          <main className="flex-1 overflow-auto bg-gray-100 p-6 md:p-10">
+            <Outlet /> {/* ✅ renders nested route components */}
+          </main>
+        </div>
       </div>
     </div>
   );
